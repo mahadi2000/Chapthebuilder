93d091cb47336340456ff3be8621743c317e8228
mahadi2000 <mah.dragon0063@gmail.com>
2024-02-01 15:30:37 +0500

    first commit

---
+++ js/Home.js
@@ -0,0 +1,50 @@
+
+function toggleMenu() {
+  const menu = document.getElementById("res_menu")
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
+
+function toggleResMenu() {
+  const resmenu = document.getElementById("res_dropdown")
+  if (resmenu.style.display === "none") {
+    resmenu.style.display = "block";
+  } else {
+    resmenu.style.display = "none";
+  }
+}
+
+
+
+$(document).ready(function () {
+  $('.center').slick({
+    centerMode: true,
+    autoplay: false,
+    arrows: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0',
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: false,
+          centerPadding: '0',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
---
+++ js/Nav.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const menu = document.getElementById("res_menu")
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
+  
+  function toggleResMenu() {
+    const resmenu = document.getElementById("res_dropdown")
+    if (resmenu.style.display === "none") {
+      resmenu.style.display = "block";
+    } else {
+      resmenu.style.display = "none";
+    }
+  }
